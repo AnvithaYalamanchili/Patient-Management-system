d64998a64b682cb1394e974b2649c1bab894a5ba
AnvithaYalamanchili <anvithayalamanchili3@gmail.com>
2025-07-15 13:24:43 -0500

    Added jwt

--- router/auth.py
+++ router/auth.py
@@ -4,17 +4,17 @@ from database import get_db
 import models
 import utils
 import schemas
+import oauth2
 
 router=APIRouter(tags=["Authentication"])
 
 @router.post('/login')
 def login(user:schemas.PatientUserResponse,db:Session=Depends(get_db)):
-    try:
+
         login=db.query(models.PatientUser).filter(models.PatientUser.email==user.email).first()
         if not login:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Credentials")
         if not utils.verify(user.password,login.password):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid Credentials")
-    except Exception as e:
-        print(e)
-    return {"token":"example"}
+        access_token=oauth2.create_access_token(data={"user_id":login.id})
+        return {"access_token":access_token,"token_type":"bearer"}
--- schemas.py
+++ schemas.py
@@ -24,6 +24,7 @@ class PatientUser(BaseModel):
 class PatientUserResponse(BaseModel):
     name:str
     email:EmailStr
+    password:str
     class Config:
         orm_mode=True
         pass
